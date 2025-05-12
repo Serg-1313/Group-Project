da2492511e9358e9af1be96155409cd1a32f890f
Sergey <sergey_martirosyan2@edu.aua.am>
2025-05-12 20:07:58 +0400

    changes

--- src/main/java/am/aua/UiController.java
+++ src/main/java/am/aua/UiController.java
@@ -3,7 +3,7 @@ package am.aua;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class UiController {
     @FXML
     private Label welcomeText;
 
